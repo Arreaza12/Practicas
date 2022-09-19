8ec909e19867b56e2f714745cd1db86486cc35d7
Cruz Arreaza <109703620+CruzArreaza@users.noreply.github.com>
2022-09-18 21:19:29 -0500

    Add files via upload

---
+++ proyecto/main.js
@@ -0,0 +1,94 @@
+const poke_container = document.getElementById(`poke_container`);
+const pokemons_number = 151;
+
+const d = document,
+  $pokemones = d.getElementById("pokemones"),
+  $template = d.getElementById("pokemon-template").content,
+  $fragment = d.createDocumentFragment();
+
+//con este evento se podra darle click a la carta para poder agrandarla
+const overlay = document.getElementById("overlay");
+
+overlay.addEventListener("click", () => {
+  overlay.classList.toggle("overlay-hidden");
+});
+
+// se realizo un buscador dentro de la pagina para asi poder buscar mediante el nombre del pokemon a cada uno
+const input = document.getElementById("buscador");
+
+input.addEventListener("change", async (e) => {
+  let query = e.target.value.toLowerCase();
+
+  if (query.trim().length === 0) {
+    cleanPokemons();
+    return fetchPokemons();
+  }
+
+  const api = `https://pokeapi.co/api/v2/pokemon/${query}`;
+  const res = await fetch(api);
+  const json = await res.json();
+
+  cleanPokemons();
+  createPokemonCard(json);
+});
+
+const fetchPokemons = async () => {
+  for (let i = 1; i < pokemons_number; i++) {
+    await getPokemons(i);
+  }
+};
+
+const getPokemons = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const answer = await fetch(url);
+  const pokemon = await answer.json();
+  createPokemonCard(pokemon);
+};
+
+const createPokemonCard = (pokemon) => {
+  const { name, types, sprites, id } = pokemon;
+  const type = types[0].type.name;
+  const pokemonElement = document.createElement("div");
+  pokemonElement.classList.add("pokemon");
+  pokemonElement.classList.add("grow");
+  const pokeInnerHtml = `
+  <div class='img-container poke-card' onclick="findPokemon(${name})" id='${name}'>
+      <img src='${sprites.front_default}' alt='${name}'/>
+    
+      </div>
+      <div class='info'>
+      <span class='number'>${id}</span>
+      <h3 class='name'>${name}</h3> 
+      <small class='${type}'>${type}</small>
+      
+      </div>`;
+
+  pokemonElement.innerHTML = pokeInnerHtml;
+  poke_container.appendChild(pokemonElement);
+};
+
+const cleanPokemons = () => {
+  poke_container.innerHTML = "";
+};
+
+//con este evento se podra darle click a la carta para poder agrandarla
+const findPokemon = async (card) => {
+  const overlay = document.getElementById("overlay");
+  const modal = document.getElementById("modal");
+
+  overlay.classList.toggle("overlay-hidden");
+  const url = `https://pokeapi.co/api/v2/pokemon/${card.id}`;
+  const answer = await fetch(url);
+  const pokemon = await answer.json();
+
+  modal.innerHTML = `
+    <div>
+      <img src='${pokemon.sprites.front_default}' alt='${name}'/>
+      
+    </div>
+    
+    
+    `;
+};
+
+fetchPokemons();
